a6ae22c7bd0109552af76264d820663e10be6b5c
raniel54 <ranielsilva65@hotmail.com>
2024-04-03 17:16:28 -0300

    correção de testes

--- src/models/repository/events_repository_test.py
+++ src/models/repository/events_repository_test.py
@@ -4,12 +4,12 @@ from .events_repository import EventsRepository
 
 db_connection_handler.connect_to_db()
 
-# @pytest.mark.skip(reason="Novo registro em banco de dados")
+@pytest.mark.skip(reason="Novo registro em banco de dados")
 def test_insert_event():
     event = {
-        "uuid": "meu-uuid-e-nois",
+        "uuid": "meu-uuid-e-nois2",
         "title": "meu title",
-        "slug": "meu-slug-aqui!",
+        "slug": "meu-slug-aqui!2",
         "maximum_attendees": 20
     }
 
@@ -17,10 +17,11 @@ def test_insert_event():
     response = events_repository.insert_event(event)
     print(response)
 
-# @pytest.mark.skip(reason="Nao necessita")
-# def test_get_event_by_id():
-#     event_id = "meu-uuid-e-nois2"
-#     events_repository = EventsRepository()
-#     response = events_repository.get_event_by_id(event_id)
-#     print(response)
-#     print(response.title)
+@pytest.mark.skip(reason="Nao necessita")
+def test_get_event_by_id():
+    event_id = "meu-uuid-e-nois2"
+    events_repository = EventsRepository()
+    response = events_repository.get_event_by_id(event_id)
+    print(response)
+    print(response.title)
+    print(response.title)
